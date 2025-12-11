6a8fb7bfadec988c947c84619a3cbd020069ca69
James Slomka <james.slomka@ssense.com>
2025-12-11 15:55:05 -0500

    cache handler

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+   cacheHandlers: {
+      default: require.resolve("./src/lib/cache/cache-handler.cjs"),
+      remote: require.resolve("./src/lib/cache/cache-handler.cjs"),
+    },
 };
 
 export default nextConfig;
